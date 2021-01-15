5bc56c627b992189cb4f0380ba05ad56bfc764df
jazibghori <77227927+jazibghori@users.noreply.github.com>
2021-01-15 15:51:49 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.temperature())
+    basic.showNumber(add(68, 50))
 })
 function convertc2f (c: number) {
     return input.temperature() * 1.8 + 32
@@ -7,3 +7,6 @@ function convertc2f (c: number) {
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(convertc2f(input.temperature()))
 })
+function add (num1: number, num2: number) {
+    return num1 + num2
+}
